9c8097c043f3cd27a1011ea1046356286c08007e
Kauan Dos Anjos Vieira <kauaanvieiraa07@gmail.com>
2025-05-06 16:51:13 -0300

    Revert "Atualiza├º├úo"
    This reverts commit 1fb3ba3a3670221681a05036f6600d8f2755e03a.

--- sync_to_repo.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-xcopy /E /H /Y /I "C:\xampp\htdocs\CONEXAO_SCRIPT" "C:\Users\kauan_a_vieira\Documents\GitHub\KauanVieira_DESN_Sistemas_Tarde"
-cd "C:\Users\kauan_a_vieira\Documents\GitHub\KauanVieira_DESN_Sistemas_Tarde"
-git add .
-git commit -m "Atualização"
-git push origin main
-pause
