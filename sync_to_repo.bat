7f17bbe51285b50710ae055980afb93103064bbb
Kauan Dos Anjos Vieira <kauaanvieiraa07@gmail.com>
2025-05-13 13:57:00 -0300

    Atualizando do conteudo

--- sync_to_repo.bat
+++ sync_to_repo.bat
@@ -1,5 +1,5 @@
 @echo off
-xcopy /E /H /Y /I "C:\Users\kauan_a_vieira\Documents\GitHub\KauanVieira_DESN_Sistemas_Tarde"
+xcopy /E /H /Y /I "C:\xampp\htdocs\KauanVieira_PDO"
 cd "C:\Users\kauan_a_vieira\Documents\GitHub\KauanVieira_DESN_Sistemas_Tarde"
 git add .
 git commit -m "Atualizando do conteudo"
