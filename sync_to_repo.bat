3501c501d3272467e3498378acff0e99a2ee2601
Kauan Dos Anjos Vieira <kauaanvieiraa07@gmail.com>
2025-05-14 17:10:50 -0300

    att

--- sync_to_repo.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-xcopy /E /H /Y /I "C:\xampp\htdocs\KauanVieira_SQLINJECT"
-cd "C:\Users\kauan_a_vieira\Documents\GitHub\KauanVieira_DESN_Sistemas_Tarde"
-git add .
-git commit -m "Atualizando conteudo"
-git push origin main
-pause
