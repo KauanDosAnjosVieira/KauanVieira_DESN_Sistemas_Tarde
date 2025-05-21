683fe8a001f08b27de3ffc76c1e63e7dc07a3ef4
Kauan Dos Anjos Vieira <kauaanvieiraa07@gmail.com>
2025-05-21 17:04:07 -0300

    Atualizando conteudo

--- KauanVieira_Login/sync_to_repo.bat
+++ KauanVieira_Login/sync_to_repo.bat
@@ -1,5 +1,5 @@
 @echo off
-xcopy /E /H /Y /I "C:\xampp\htdocs\KauanVieira_login"
+xcopy /E /H /Y /I "C:\xampp\htdocs\KauanVieira_Login" "C:\Users\kauan_a_vieira\Documents\GitHub\KauanVieira_DESN_Sistemas_Tarde/KauanVieira_Login"
 cd "C:\Users\kauan_a_vieira\Documents\GitHub\KauanVieira_DESN_Sistemas_Tarde/KauanVieira_Login"
 git add .
 git commit -m "Atualizando conteudo"
